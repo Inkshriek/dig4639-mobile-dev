750d911495855d83fee18897ed754dc79ebd202d
Noah Jervey <no518143@ucf.edu>
2020-02-03 12:11:05 -0500

    app 78

--- ice/w5a/src/components/Task.js
+++ ice/w5a/src/components/Task.js
@@ -5,12 +5,15 @@ class Task {
         this.element = document.createElement("div");
         let checkbox = document.createElement("input");
         checkbox.type = "checkbox";
+        checkbox.addEventListener("change", this.inputChanged);
         this.element.appendChild(checkbox);
         let taskText = document.createElement("span");
         taskText.innerHTML = props.content;
         this.element.appendChild(taskText);
     }
-
+    inputChanged() {
+        console.log(this.element.innerHTML);
+    }
     render () {
         return this.element;
     }
