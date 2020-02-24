6496385f7b21ce658a9f7a88601564bb72f820d4
Inkshriek <inkshriek@gmail.com>
2020-02-24 12:17:03 -0500

    w8a

---
+++ demo/w8a/index.js
@@ -0,0 +1,8 @@
+const myArray = [2, 5, 8, 20, 18];
+console.log(myArray);
+
+let sum = 0;
+for (let index = 0; index < myArray.length; index++) {
+    sum = sum + myArray[index];
+}
+console.log(sum);
